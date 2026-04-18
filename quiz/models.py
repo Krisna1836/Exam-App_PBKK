68e2d6867926de4b1109760775ee761d3d6b54ef
KhalidLumbaLumba <kakadiqi06@gmail.com>
2026-04-18 15:57:19 +0700

    Adding Backend

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class QuizHistory(models.Model):
+    username = models.CharField(max_length=100)
+    material = models.CharField(max_length=50)
+    score = models.IntegerField()
+    correct = models.IntegerField(default=0)
+    wrong = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username} - {self.material} - {self.score}"
+
+    class Meta:
+        ordering = ['-created_at']
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .data.questions import questions_data
+from .models import QuizHistory
+
 
 def home(request):
     materials = [
@@ -25,8 +27,44 @@ def quiz(request, material):
 
 
 def result(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', 'Anonymous')
+        material = request.POST.get('material', '')
+        questions = questions_data.get(material, [])
+
+        correct = 0
+        wrong = 0
+
+        for q in questions:
+            submitted_answer = request.POST.get(f'q{q["id"]}', '')
+            if submitted_answer == q['answer']:
+                correct += 1
+            else:
+                wrong += 1
+
+        total = correct + wrong
+        score = round((correct / total) * 100) if total > 0 else 0
+
+        QuizHistory.objects.create(
+            username=username,
+            material=material,
+            score=score,
+            correct=correct,
+            wrong=wrong,
+        )
+
+        return render(request, 'result.html', {
+            'username': username,
+            'material': material,
+            'correct': correct,
+            'wrong': wrong,
+            'score': score,
+            'total': total,
+        })
+
     return render(request, 'result.html')
 
 
 def history(request):
-    return render(request, 'history.html')
+    histories = QuizHistory.objects.all()
+    return render(request, 'history.html', {'histories': histories})
